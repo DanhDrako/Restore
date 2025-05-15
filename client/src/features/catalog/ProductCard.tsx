d851122eee138f73b53d49a9b7bede5b1529ba3d
Danh Doan <nkocdanh58@gmail.com>
2025-05-15 21:54:15 +0700

    Implement Routing in React

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -4,9 +4,10 @@ import {
   CardActions,
   CardContent,
   CardMedia,
-  Typography,
-} from "@mui/material";
-import type { Product } from "../../app/models/product";
+  Typography
+} from '@mui/material';
+import type { Product } from '../../app/models/product';
+import { Link } from 'react-router';
 
 type Props = {
   product: Product;
@@ -19,31 +20,33 @@ export default function ProductCard({ product }: Props) {
       sx={{
         width: 280,
         borderRadius: 2,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
       }}
     >
       <CardMedia
-        sx={{ height: 240, backgroundSize: "cover" }}
+        sx={{ height: 240, backgroundSize: 'cover' }}
         image={product.pictureUrl}
         title={product.name}
       />
       <CardContent>
         <Typography
           gutterBottom
-          sx={{ textTransform: "uppercase" }}
+          sx={{ textTransform: 'uppercase' }}
           variant="subtitle2"
         >
           {product.name}
         </Typography>
-        <Typography sx={{ color: "secondary.main" }} variant="h6">
+        <Typography sx={{ color: 'secondary.main' }} variant="h6">
           ${(product.price / 100).toFixed(2)}
         </Typography>
       </CardContent>
-      <CardActions sx={{ justifyContent: "space-between" }}>
+      <CardActions sx={{ justifyContent: 'space-between' }}>
         <Button>Add to card</Button>
-        <Button>View</Button>
+        <Button component={Link} to={`/catalog/${product.id}`}>
+          View
+        </Button>
       </CardActions>
     </Card>
   );
