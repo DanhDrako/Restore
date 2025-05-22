31729839746f0f17484e4a3a204b5364cd0df50a
Danh Doan <nkocdanh58@gmail.com>
2025-05-22 09:10:12 +0700

    Implement get/add/update/delete item to the basket

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,97 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class BasketController(StoreContext context) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<BasketDto>> GetBasket()
+        {
+            var basket = await RetrieveBasket();
+
+            if (basket == null) return NoContent();
+
+            return basket.ToDto();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId, int quantity)
+        {
+            // get basket
+            var basket = await RetrieveBasket();
+            // create basket if it doesn't exist
+            basket ??= CreateBasket();
+
+            // get product
+            var product = await context.Products.FindAsync(productId);
+            if (product == null) return BadRequest("Problem adding item to basket");
+
+            // add item to basket
+            basket.AddItem(product, quantity);
+
+            // save changes
+            var result = await context.SaveChangesAsync() > 0;
+            if (result) return CreatedAtAction(nameof(GetBasket), basket.ToDto());
+
+            return BadRequest("Problem updating basket");
+        }
+
+
+
+        [HttpDelete]
+        public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+        {
+            // get basket
+            var basket = await RetrieveBasket();
+
+            // remove the item or reduce its quantity
+            if (basket == null) return BadRequest("Unable to retrieve basket");
+
+            // remove item from basket
+            basket.RemoveItem(productId, quantity);
+
+            // save changes
+            var result = await context.SaveChangesAsync() > 0;
+            if (result) return Ok();
+
+            return BadRequest("Problem updating basket");
+        }
+
+        private async Task<Basket?> RetrieveBasket()
+        {
+            return await context.Baskets
+                .Include(x => x.Items)
+                .ThenInclude(x => x.Product)
+                .FirstOrDefaultAsync(x => x.BasketId == Request.Cookies["basketId"]);
+        }
+
+        private Basket CreateBasket()
+        {
+            // generate a new basket id
+            var basketId = Guid.NewGuid().ToString();
+
+            // set cookie
+            var cookieOptions = new CookieOptions
+            {
+                IsEssential = true,
+                Expires = DateTime.UtcNow.AddDays(30),
+            };
+            Response.Cookies.Append("basketId", basketId, cookieOptions);
+
+            // create basket
+            var basket = new Basket
+            {
+                BasketId = basketId
+            };
+            // add basket to context
+            context.Baskets.Add(basket);
+
+            return basket;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -15,6 +15,9 @@ microsoft.entityframeworkcore.sqlserver
 -- dotnet ef migrations add InitialCreate -o Data/Migrations
 -- dotnet ef database update
 
+-want to delete migrations added ?
+-- dotnet ef migrations remove
+
 -want to restore all database?
 -- dotnet ef database drop
 ```
